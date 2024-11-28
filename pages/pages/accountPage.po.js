7a61eef1c48bc10520ee37175d3fa30500104efc
karegowd <karegowd1667@gmail.com>
2024-11-28 20:53:49 +0530

    Magetha task

---
+++ pages/pages/accountPage.po.js
@@ -0,0 +1,17 @@
+class accountPage {
+    assertAccountPage() {
+        return "https://magento.softwaretestingboard.com/customer/account/"
+    }
+    clickonArrow() {
+        return '//button[@type="button"]'
+    }
+
+    clickOnSignOut() {
+        return 'Sign Out '
+    }
+    
+}
+
+
+const obj_accountPage = new accountPage()
+export default obj_accountPage
